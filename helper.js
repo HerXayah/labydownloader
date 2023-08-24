253c2a5894c255d593e5270e3dfb71f8a72d2e2d
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2023-08-24 17:23:28 +0200

    add stand for now

---
+++ helper.js
@@ -0,0 +1,16 @@
+export function FixManifest(Manifest) {
+  // read manifest as json
+  let ManifestJSON = JSON.parse(Manifest); // get all versions
+}
+
+
+async function fixDownloads(ManifestJSON) {
+  ManifestJSON.downloads.forEach(download => {
+    // if download isnt "client", remove the entry
+    if (download.name != "client") {
+      ManifestJSON.downloads.splice(ManifestJSON.downloads.indexOf(download), 1);
+    }
+  });
+  return ManifestJSON;
+}
+
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-import { text, intro, outro, spinner, confirm  } from '@clack/prompts';
+import { text, intro, outro, spinner, confirm, select  } from '@clack/prompts';
 import fs from 'fs';
 import https from 'https';
 
+
+let minecraftJson = {
+    "name": `net.labymod:${jsonLabyName}`,
+    "url":"https://releases.r2.labymod.net/libraries/",
+    "rules": [
+    ],
+};
+
+
 intro('LabyMod Downloader');
-let downloadString = `https://laby-releases.s3.de.io.cloud.ovh.net/api/v1/download/labymod4/`;
+
+const s = spinner();
+
+let labyVersions = await select({
+    message: 'Select the Branch you want to download.',
+    options: [
+      { value: 'snapshot', label: 'Snapshot' },
+      { value: 'production', label: 'Release' },
+    ],
+    required: true,
+});
+
+// populating versions
+let downloadString = `https://laby-releases.s3.de.io.cloud.ovh.net/api/v1/download/labymod4/${labyVersions.toString()}/`;
 let version = await text({
     message: 'What version of LabyMod do you want to download?',
-    placeholder: 'e.g. snapshot/49206d8e',
+    placeholder: 'e.g. 49206d8e',
     validate(value) {
         if (value.length === 0)
             return `Value is required!`;
@@ -18,7 +40,7 @@ let version = await text({
 downloadString = downloadString.toString() + version.toString() + ".jar";
 let downloadPath = await text({
     message: 'Now enter the path to your MultiMC/Prism Instance Folder',
-    placeholder: 'e.g. C:/Users/Admin/AppData/Roaming/PrismLauncher/instances/LabyMod-4-1.19.4/libraries',
+    placeholder: 'e.g. C:/Users/Admin/AppData/Roaming/PrismLauncher/instances/LabyMod-4-1.19.4',
     validate(value) {
         value.trim();
         if (value.length === 0)
@@ -39,9 +61,9 @@ function checkIfFileExists(path) {
         console.error(err);
     }
 }
-async function downloadFile(url, targetFile) {
-    s.message('Downloading LabyMod-4.jar');
-    return await new Promise((resolve, reject) => {
+async function downloadFile(url, targetFile, filename) {
+    s.message('Downloading ' + filename);
+    return new Promise((resolve, reject) => {
         https.get(url, response => {
             if (response.statusCode === 200) {
                 const file = fs.createWriteStream(targetFile);
@@ -49,39 +71,86 @@ async function downloadFile(url, targetFile) {
                 file.on('finish', () => {
                     file.close(() => {
                         resolve(true);
-                        s.message('Finished Downloaded LabyMod-4.jar');
+                        s.message('Finished Downloading ' + filename);
                     });
                 });
             } else {
-                reject(false);
-                s.message('Finished Downloaded LabyMod-4.jar');
+                reject(new Error('Error while downloading ' + filename));
+                s.message('Error while downloading ' + filename);
             }
         }).on('error', error => {
             reject(error);
-            s.message('Error while downloading LabyMod-4.jar');
+            s.message('Error while downloading ' + filename);
         });
     });
 }
 
-const s = spinner();
+let manifest = await new Promise((resolve, reject) => {
+    // get manifest from url
+    https.get(`https://laby-releases.s3.de.io.cloud.ovh.net/api/v1/manifest/${labyVersions.toString()}/latest.json`, response => {
+        if (response.statusCode === 200) {
+            let data = '';
+            response.on('data', chunk => {
+                data += chunk;
+            });
+            response.on('end', () => {
+                resolve(JSON.parse(data));
+            });
+        } else {
+            reject(false);
+            s.message('Error while downloading manifest')
+        }
+    }).on('error', error => {
+        reject(error);
+        s.message('Error while downloading manifest')
+    });
+});
 
-s.start('Checking if LabyMod-4.jar exists');
-downloadPath = downloadPath.toString() + "/";
-// remove \\ and replace with /
-downloadPath = downloadPath.replace(/\\/g, "/");
-if (checkIfFileExists(downloadPath.toString() + "LabyMod-4.jar") == true) {
+let versions = [];
+for (let i = 0; i < manifest.minecraftVersions.length; i++) {
+    versions.push({ value: manifest.minecraftVersions[i].version, label: manifest.minecraftVersions[i].tag });
+}
+
+let mcVersions = await select({
+    message: 'Select additional tools.',
+    options: JSON.parse(JSON.stringify(versions)),
+    required: true,
+  });
+
+  downloadPath = downloadPath.toString() + "/";
+  // remove \\ and replace with /
+  downloadPath = downloadPath.replace(/\\/g, "/");
+  
+s.start('Processing Minecraft JSON');
+
+try {
+    await processMinecraftJson();
+    s.message('Processed Minecraft JSON');
+} catch (error) {
+    s.message('Error while processing Minecraft JSON');
+    console.error(error);
+    process.exit(1);
+}
+
+s.message('Checking if LabyMod-4.jar exists');
+if (checkIfFileExists(downloadPath.toString() + "libraries/LabyMod-4.jar") == true) {
     // delete file
     const shouldContinue = await confirm({
         message: 'Do you want to backup the old LabyMod-4.jar?',
     });
     if (shouldContinue) {
-        await fs.renameSync(downloadPath.toString() + "LabyMod-4.jar", downloadPath.toString() + "LabyMod-4.jar.old");
+        await fs.renameSync(downloadPath.toString() + "/libraries/LabyMod-4.jar", downloadPath.toString() + "/libraries/LabyMod-4.jar.old");
     }
     s.message('Renamed LabyMod-4.jar to LabyMod-4.jar.old');
 }
 
 s.message('LabyMod-4.jar does not exist');
-await downloadFile(downloadString, downloadPath.toString() + "LabyMod-4.jar");
+// if folder does not exist create it
+if (!fs.existsSync(downloadPath.toString() + "libraries")) {
+    fs.mkdirSync(downloadPath.toString() + "libraries");
+    s.message('Created libraries folder');
+}
+//await downloadFile(downloadString, downloadPath.toString() + "/libraries/LabyMod-4.jar", "LabyMod-4.jar");
 s.stop();
 
 outro('LabyMod Downloader complete!');
