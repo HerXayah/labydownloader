e9b6cf6f28216c501c8b863bbd7cd3fdcad5bfe3
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2023-09-05 01:37:15 +0200

    my, oh my, look at who ends up bigger this time.

--- downloadHelper.js
+++ downloadHelper.js
@@ -27,50 +27,56 @@ export async function downloadFile(url, targetFile, s) {
     });
   }
 
-export async function downloadLabyVersions() {
-    let file = await new Promise((resolve, reject) => {
-        // get manifest from url
-        https.get(`https://releases.r2.labymod.net/api/v1/channels.json`, response => {
-            if (response.statusCode === 200) {
-                let data = '';
-                response.on('data', chunk => {
-                    data += chunk;
-                });
-                response.on('end', () => {
-                    resolve(JSON.parse(data));
-                });
-            } else {
-                reject(false);
-                s.message('Error while downloading manifest')
-            }
-        }).on('error', error => {
-            reject(error);
-            s.message('Error while downloading manifest')
-        });
+  async function makeHttpRequest(url) {
+    return new Promise((resolve, reject) => {
+      https.get(url, (response) => {
+        if (response.statusCode === 200) {
+          let data = '';
+          response.on('data', (chunk) => {
+            data += chunk;
+          });
+          response.on('end', () => {
+            resolve(JSON.parse(data));
+          });
+        } else {
+          reject(new Error('Error while downloading manifest'));
+        }
+      }).on('error', (error) => {
+        reject(error);
+      });
     });
-    return file;
-}
+  }
+  
+  export async function downloadLabyVersions() {
+    return await makeHttpRequest('https://releases.r2.labymod.net/api/v1/channels.json');
+  }
+  
+  export async function downloadSpecificMC(channel) {
+    return await makeHttpRequest(`https://laby-releases.s3.de.io.cloud.ovh.net/api/v1/manifest/${channel}/latest.json`);
+  }
 
-export async function downloadSpecificMC(channel) {
-    let file = await new Promise((resolve, reject) => {
-        // get manifest from url
-        https.get(`https://laby-releases.s3.de.io.cloud.ovh.net/api/v1/manifest/${channel}/latest.json`, response => {
-            if (response.statusCode === 200) {
-                let data = '';
-                response.on('data', chunk => {
-                    data += chunk;
-                });
-                response.on('end', () => {
-                    resolve(JSON.parse(data));
-                });
-            } else {
-                reject(false);
-                s.message('Error while downloading manifest')
-            }
-        }).on('error', error => {
-            reject(error);
-            s.message('Error while downloading manifest')
-        });
+  export async function downloadAssets(channel, commitReference, name, checksum, s, targetFile) {
+    s.message('Downloading LabyMod-4.jar');
+    return await new Promise((resolve, reject) => {
+      https
+        .get(`https://releases.r2.labymod.net/api/v1/download/assets/labymod4/${channel}/${commitReference}/${name}/${checksum}.jar`, (response) => {
+          if (response.statusCode === 200) {
+            const file = fs.createWriteStream(targetFile);
+            response.pipe(file);
+            file.on('finish', () => {
+              file.close(() => {
+                resolve(true);
+                s.message(`Finished downloading ${name}`);
+              });
+            });
+          } else {
+            reject(false);
+            s.message(`Finished downloading ${name}`);
+          }
+        })
+        .on('error', (error) => {
+          reject(error);
+          s.message(`Error while downloading ${name}`);
+        });s
     });
-    return file;
-}
+  }
